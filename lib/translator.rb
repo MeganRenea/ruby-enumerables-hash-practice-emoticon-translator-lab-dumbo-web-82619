bce15c920abb8f983f5ebf5c332210afac11674a
Megan Edmonds <meganr.edmonds@gmail.com>
2019-07-25 20:34:17 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,12 @@ require "pry"
 
 def load_library(library)
   library = YAML.load_file(library)
-  binding.pry
+  library = library.reduce({}) do |memo, (key,value)|
+    memo["get_meaning"] = library.keys
+    memo["get_emoticon"] = library.values
+    memo
+  end
+  library
 end
 
 def get_japanese_emoticon
