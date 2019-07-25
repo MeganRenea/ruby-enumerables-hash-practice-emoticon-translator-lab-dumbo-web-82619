8da57f6efe9081d94dd54bbd294d77f79e11024a
Megan Edmonds <meganr.edmonds@gmail.com>
2019-07-25 21:22:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,6 +24,6 @@ def get_japanese_emoticon (library, emoticon)
     return "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning (library, emoticon)
+  
 end
