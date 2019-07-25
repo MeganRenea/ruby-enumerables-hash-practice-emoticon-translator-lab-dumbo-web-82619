eaa8b5cda656066d9614f3ce4f80bf7f85fab62f
Megan Edmonds <meganr.edmonds@gmail.com>
2019-07-25 20:43:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,8 +4,10 @@ require "pry"
 def load_library(library)
   library = YAML.load_file(library)
   library = library.reduce({}) do |memo, (key,value)|
-    memo["get_meaning"] = {key => nil}
-    memo["get_emoticon"] = {value => nil}
+    japanese = value[1]
+    english = value[0]
+    memo["get_meaning"] = {key => japanese}
+    memo["get_emoticon"] = {value => english}
     memo
     binding.pry
   end
