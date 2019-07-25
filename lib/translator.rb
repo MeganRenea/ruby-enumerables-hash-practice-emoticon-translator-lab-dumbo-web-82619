18c3f39a34b06037af4c8b7feb8636e642239f66
Megan Edmonds <meganr.edmonds@gmail.com>
2019-07-25 21:06:56 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@ require "pry"
 
 def load_library(library)
   library = YAML.load_file(library)
-  new = {:get_meaning => nil, :get_emoticon => nil}
+  new = {:get_meaning => {}, :get_emoticon => {}}
   #binding.pry
   library.each do |(key,value)|
     japanese = value[1]
